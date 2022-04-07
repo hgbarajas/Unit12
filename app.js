b1376eb6acc70fe695d701e871645eff57681191
Barajas <hbarajas@FVFZMYB5J1WK.tld>
2022-04-07 13:30:59 -0500

    second commit

--- app.js
+++ app.js
@@ -22,23 +22,11 @@ function run() {
 
 function randomNum() {
     let ran = Math.floor(Math.random() * 10);
-    let x = document.getElementById("random");
-    x.innerHTML = ran;
-    x.style.backgroundColor = "#280cc1";
-    x.style.color = "white";
-    x.style.padding = "20px";
-    x.style.textAlign = "center";
     return ran;
 }
 
 function userNumber() {
     let user = document.getElementById("getNumber").value;
-    let y = document.getElementById("userNumber")
-    y.innerHTML = user;
-    y.style.color = "white";
-    y.style.backgroundColor = "#ff00e2";
-    y.style.padding = "20px";
-    y.style.textAlign = "center";
     return user;
 }
 
